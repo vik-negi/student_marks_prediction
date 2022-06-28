427410d9401cace4632068b418ac855268dd8839
vikram negi <vikramnegi175@gmail.com>
2022-06-29 00:07:59 +0530

    commit

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ model = joblib.load("students_marks_predictor.pkl")
 df = pd.DataFrame()
 
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
 
 @app.route('/predict',methods=['POST'])
